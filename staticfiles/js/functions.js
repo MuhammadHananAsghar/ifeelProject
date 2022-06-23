aa970688f422a7215e0bb8f64ffe180110e73f7a
sultan <muhammadhananasghar@gmail.com>
2022-06-24 00:45:55 +0500

    deploy try 4

---
+++ staticfiles/js/functions.js
@@ -0,0 +1,90 @@
+const searchBtn = document.getElementById("searchButton");
+
+searchBtn.addEventListener("click", () => {
+  const search = document.querySelector("#search");
+  if (search.value) {
+    window.location.href = "/main/" + search.value;
+  } else {
+    alert("Please enter something to search...");
+  }
+});
+
+// Migration Functions
+function migrate(address){
+    window.location.href = "/main/"+address;
+}
+
+// Load Songs
+function loadSong(lyrics_id, subtitle, id, enc_url, title, image, csrf_token) {
+  var data = {
+    lyrics_id: lyrics_id,
+    subtitle: subtitle,
+    id: id,
+    enc_url: enc_url,
+    title: title,
+    image: image,
+  };
+
+  $.ajax({
+    type: "POST",
+    url: "/main/song/",
+    data: { csrfmiddlewaretoken: csrf_token, data: data },
+    success: function callback(response) {
+      const audio = document.querySelector("audio");
+      const image = document.getElementById("songImage");
+      const title = document.getElementById("songTitle");
+      const subtitle = document.getElementById("songDesc");
+
+      localStorage.setItem("songurl", response["songurl"]);
+      localStorage.setItem("image", response["image"]);
+      localStorage.setItem("title", response["title"]);
+      localStorage.setItem("subtitle", response["subtitle"]);
+      audio.src = response["songurl"];
+      image.src = response["image"];
+      title.textContent = response["title"];
+      subtitle.textContent = response["subtitle"];
+      document.title = "Now Playing - "+response["title"];
+    },
+  });
+}
+
+if (localStorage.getItem("songurl")) {
+  const audio = document.querySelector("audio");
+  const image = document.getElementById("songImage");
+  const title = document.getElementById("songTitle");
+  const subtitle = document.getElementById("songDesc");
+
+  audio.src = localStorage.getItem("songurl");
+  image.src = localStorage.getItem("image");
+  title.textContent = localStorage.getItem("title");
+  subtitle.textContent = localStorage.getItem("subtitle");
+  document.title = "Now Playing - "+localStorage.getItem("title");
+}
+// Downloader Function
+function downloadFileWithLink(href) {
+  var link = document.createElement("a");
+  let name = (href?.split("/") || [])
+  name = name[name?.length-1]
+  link.setAttribute('download', name);
+  link.href = href;
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+}
+
+// Download
+function download(id, url, csrf_token){
+  var data = {
+    id: id,
+    enc_url: url
+  };
+
+  $.ajax({
+    type: "POST",
+    url: "/main/download/",
+    data: { csrfmiddlewaretoken: csrf_token, data: data },
+    success: function callback(response) {
+      downloadFileWithLink(response['songurl']);
+    },
+  });
+}
---
+++ staticfiles/js/player.121739b3ece1.js
@@ -0,0 +1,94 @@
+/** Implementation of the functionality of the audio player */
+const playBtn = document.getElementById("play-icon");
+const volumeBtn = document.getElementById("volume-button");
+const seekSlider = document.getElementById("seek-slider");
+const volumeSlider = document.getElementById("volumeSlider");
+const audio = document.querySelector("audio");
+const durationContainer = document.getElementById("duration");
+const currentTimeContainer = document.getElementById("current-time");
+
+playBtn.onclick = () => {
+  if (playBtn.className === "uil uil-play") {
+    playBtn.className = "uil uil-pause";
+  } else {
+    playBtn.className = "uil uil-play";
+  }
+};
+
+volumeBtn.onclick = () => {
+  let vContainer = document.querySelector(".rangeContainer");
+  if (vContainer.style.opacity === "" || vContainer.style.opacity === "0") {
+    vContainer.style.opacity = "1";
+  } else {
+    vContainer.style.opacity = "0";
+  }
+};
+
+// Calculating Time
+const calculateTime = (secs) => {
+  const minutes = Math.floor(secs / 60);
+  const seconds = Math.floor(secs % 60);
+  const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  return `${minutes}:${returnedSeconds}`;
+};
+
+// Displaying Duration
+const displayDuration = () => {
+  const durationContainer = document.getElementById("duration");
+  durationContainer.textContent = calculateTime(audio.duration);
+};
+
+// Setting Seek Slider max to
+const setSliderMax = () => {
+  seekSlider.max = Math.floor(audio.duration);
+};
+
+if (audio.readyState > 0) {
+  // Get Buffered Audio
+  let bufferedAmount = audio.buffered.end(audio.buffered.length - 1);
+  // Get Playable Audio
+  let seekableAmount = audio.seekable.end(audio.seekable.length - 1);
+
+  displayDuration();
+  setSliderMax();
+} else {
+  audio.addEventListener("loadedmetadata", () => {
+    displayDuration();
+    setSliderMax();
+  });
+}
+
+// Playing Audio
+playBtn.addEventListener("click", () => {
+  if (playBtn.className === "uil uil-pause") {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+});
+
+// Updating Time of Audio w.r.t seek
+audio.addEventListener("timeupdate", () => {
+  currentTimeContainer.textContent = calculateTime(seekSlider.value);
+  seekSlider.value = Math.floor(audio.currentTime);
+});
+
+// Changing seekbar if user changes
+seekSlider.addEventListener("change", () => {
+  audio.currentTime = seekSlider.value;
+});
+
+
+// Volume Functionality implmenetation
+volumeSlider.addEventListener("input", (e) => {
+  const value = e.target.value;
+  if(value == 1){
+    volumeBtn.className = "uil uil-volume-mute";
+  }else if(value < 50){
+    volumeBtn.className = "uil uil-volume-down";
+  }
+  else{
+    volumeBtn.className = "uil uil-volume-up";
+  }
+  audio.volume = value / 100;
+});
